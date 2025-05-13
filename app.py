cf161597f9cebb11fe2b31fe95b90f68b4d164a1
Thoriq186 <thoriq.ahsan01@gmail.com>
2025-05-13 22:57:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import math
 st.set_page_config(page_title="Metode 1 Isokinetik Pada Emisi Tidak Bergerak", layout="centered")
 
 # Title
-st.title("📏 Kalkulator Titik Sampling Pada Emisi Tidak Bergerak 💨")
+st.title(" 📏 Kalkulator Titik Sampling Pada Emisi Tidak Bergerak yang berada di industri menengah kebawah 💨
+")
 st.header(":blue[Metode 1 - Isokinetik Sampling]")
 
 st.write("""
